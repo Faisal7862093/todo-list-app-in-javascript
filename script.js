8bd8bc9337e28749038c6fd08454c690fcba94fa
Faisal7862093 <muhammadfaisal7862093@gmail.com>
2024-09-27 19:57:34 +0500

    if condition using trim() function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toDoTask() {
     var myTask = document.getElementById('myTask');
     var myTaskList = document.getElementById('myTaskList');
-    
+    if (myTask === value.trim()) {
+        return alert("Todo can't be empty!")
+    }
 
 }
