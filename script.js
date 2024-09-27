1629dab33992ec1e9143e07d6e93c4398a704aaf
Faisal7862093 <muhammadfaisal7862093@gmail.com>
2024-09-27 20:13:40 +0500

    functionality are added in list element and remove button

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function toDoTask() {
     var myTask = document.getElementById('myTask');
     var myTaskList = document.getElementById('myTaskList');
-    if (myTask === value.trim()) {
+
+    if (!myTask.value.trim()) {
         return alert("Todo can't be empty!")
     }
+
+    var li = document.createElement('li');
+    li.textContent = myTask.value;
+
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn';
+    removeButton.onclick = function () {
+        myTaskList.removeChild(li);
+    };
+
+   
+
 
 }
